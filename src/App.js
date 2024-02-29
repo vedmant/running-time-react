c302e311193008ad062663159f605f19a510c702
Vedmant <vedmant@gmail.com>
2024-02-29 22:09:26 +0700

    Fixing ios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Platform, StatusBar, View } from 'react-native'
+import { Platform, StatusBar, SafeAreaView } from 'react-native'
 import AppNavigator from './navigation/AppNavigator'
 
 export default function App () {
   return (
-    <View tw="flex-1">
+    <SafeAreaView tw="flex-1">
       {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
       <AppNavigator />
-    </View>
+    </SafeAreaView>
   )
 }
--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -9,8 +9,8 @@ export default function ({ label, icon, error, className, ...props }) {
     <View className={`mb-4 ${className}`}>
       {label && (<Text className="mb-2">{label}</Text>)}
       <View className={`border px-2 flex flex-row items-center justify-start rounded-lg bg-white ${border}`}>
-        {icon && cloneElement(icon, { weight: 'bold', size: 18 })}
-        <TextInput className="w-full" onFocus={() => setFocused(true)} onBlur={() => setFocused(false)} {...props} />
+        {icon && cloneElement(icon, { weight: 'bold', size: 18, style: { marginRight: 5 } })}
+        <TextInput className="w-full leading-none py-2.5" onFocus={() => setFocused(true)} onBlur={() => setFocused(false)} {...props} />
       </View>
       {error && <Text className="text-red-600 text-sm mt-1">{error}</Text>}
     </View>
